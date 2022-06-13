4f5c41e209e2050d5bf4b27bdafd8da7dfd17a60
ra0x1duk3 <ra0x1duk3@tuta.io>
2022-06-13 13:38:34 -0500

    finished profile but need to validate crashTests codeprefix/suffix, and additionalbuiltins

---
+++ profile/qjsengine.swift
@@ -0,0 +1,50 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//FIXME: make sure to put the following line in profile.swift profiles array
+// "qtjs": qtjsProfile
+
+import Fuzzilli
+
+let qtjsProfile (
+    processArguments: ["-reprl"],
+    processEnv: ["UBSAN_OPTIONS":"handle_segv=0"]
+    codePrefix: """
+                function placeholder(){}
+                function main() {
+                """,
+
+    codeSuffix: """
+                gc();
+                }
+                main();
+                """,
+
+    ecmaVersion: ECMAScriptVersion.es7,
+
+    // JavaScript code snippets that cause a crash in the target engine.
+    // Used to verify that crashes can be detected.
+    crashTests: ["fuzzilli('FUZZILLI_CRASH')"],
+    
+    additionalCodeGenerators: WeightedList<CodeGenerator>([]),
+    additionalProgramTemplates: WeightedList<ProgramTemplate>([]),
+    
+    disabledCodeGenerators: [],
+   
+    additionalBuiltins: [
+        "gc"                  : .function([] => .undefined),
+        "print"               : .function([.plain(.jsString)] => .undefined),
+        "placeholder"         : .function([] => .undefined),
+                        ])
+
+
