62d76b1754723b8ecc6ebb331e4c3fc5ee435a41
ra0x1duk3 <ra0x1duk3@tuta.io>
2022-06-14 11:31:28 -0500

    removed jsconsole files

---
+++ segfault.h
@@ -0,0 +1,23 @@
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
+#include <QJSEngine>
+#include <iostream>
+
+class SegFault : public QObject
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE SegFault(){
+	std::cout << "here";
+    };
+    virtual ~SegFault(){
+	    //destruct
+    };
+    Q_INVOKABLE void fault(){
+	std::cout << "shoulda segfaulted";
+	*((int*)0x41414141) = 0x1337;
+    };
+
+};
