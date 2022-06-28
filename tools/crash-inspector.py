f52746be0bf01a5bb2ca8ca93d20da1a87ae16d7
theo <thmorale@cisco.com>
2022-06-28 13:41:24 -0500

    initial draft of cron crash-inspector.py

--- tools/crash-inspector.py
+++ tools/crash-inspector.py
@@ -1,20 +1,38 @@
 import subprocess
 
-triage = open('./triage.txt' ,'r')
-exploitable = open('./crashes/exploitable/triage.txt', 'w')
-unknown = open('./crashes/unknown/triage.txt', 'w')
-not_exploitable = open('./crashes/not-exploitable/triage.txt', 'w')
-probably_exploitable = open('./crashes/prob-exploitable/triage.txt', 'w')
+# run this script as a cronjob from the instrumentation/results directory
+
+crash_dir = '/home/thmorale/qtjs/results/crashes'
+harness_path = '/home/thmorale/qtjs/fuzzer/instrumentation/tools/harness/harness'
+dupe_path = '/home/thmorale/qtjs/fuzzer/instrumentation/results/crashes/duplicates'
+triage_file = '/tmp/triage.txt'
+
+# run crashwalk
+cwtriage_cmd = ['/home/thmorale/qtjs/fuzzer/instrumentation/tools/cwtriage', '-tidy','-ignore', dupe_path, '-root', crash_dir, harness_path, '@@']
+subprocess.run(cwtriage_cmd, shell=False)
+print('[...]running crashwalk')
+
+# run cwdump
+cwdump_cmd = ['/home/thmorale/qtjs/fuzzer/instrumentation/tools/cwdump', './crashwalk.db', '>', triage_file]
+subprocess.run(cwdump_cmd, shell=False)
+print('[INFO] finished crashwalk')
+
+# seperate into files
+triage = open(triage_file ,'r')
+exploitable = open('./crashes/exploitable/triage.txt', 'a')
+unknown = open('./crashes/unknown/triage.txt', 'a')
+not_exploitable = open('./crashes/not-exploitable/triage.txt', 'a')
+probably_exploitable = open('./crashes/prob-exploitable/triage.txt', 'a')
 
 
 data = triage.readlines()
-print('read lines')
+print('...seperating results')
 
 
 for i in range(len(data)):
     line = data[i]
     if '---CRASH SUMMARY---' in line:
-        crash = line
+        crash = ''
         file_name = ''
         _hash = ''
         while not 'END SUMMARY' in line:
@@ -24,7 +42,7 @@ for i in range(len(data)):
                 file_name = file_name.replace('instrumentation/results/', '')
             i += 1
             line = data[i]
-        crash += '---END SUMMARY---'
+        crash += '---END SUMMARY---\n'
         crash.join('\n')
         new_file_name = file_name.split('-')[-1]
 
@@ -42,4 +60,7 @@ for i in range(len(data)):
             new_file_name = 'crashes/unknown/' + new_file_name
 
 
-        subprocess.run(['mv', file_name, new_file_name])
+        subprocess.run(['mv', file_name, new_file_name], shell=False)
+
+# remove the old artifacts (crashwalk,db)
+subprocess.run(['rm', 'crashwalk,db'], shell=False)
