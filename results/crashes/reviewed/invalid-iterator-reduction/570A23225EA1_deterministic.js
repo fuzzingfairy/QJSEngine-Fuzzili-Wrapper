f55461e4c9d912300908dc2621394ff43ba3a05f
ra0x1duk3 <ra0x1duk3@tuta.io>
2022-07-08 13:01:41 -0500

    updated from jamess and kyle meeting

--- results/crashes/reviewed/invalid-iterator-reduction/570A23225EA1_deterministic.js
+++ results/crashes/reviewed/invalid-iterator-reduction/570A23225EA1_deterministic.js
@@ -1,6 +1,8 @@
 function main() { 
 const v2 = [1, 1, 1, 1];
+//v5 is an iterator for the v2 vals
 const v5 = v2[Symbol.iterator];
+
 const v7 = [1, 1, 1, 1];
 // reduce the elements of v7 to a single value using the function provided by v5
 const v9 = v7["reduce"](v5);
