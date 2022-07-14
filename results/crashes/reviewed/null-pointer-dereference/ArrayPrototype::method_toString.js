ef24fd66df71126e42140cf5ff8764424bf0f9f5
ra0x1duk3 <ra0x1duk3@tuta.io>
2022-07-14 16:12:30 -0500

    reviewed not-exploitable

--- results/crashes/reviewed/null-pointer-dereference/ArrayPrototype::method_toString.js
+++ results/crashes/reviewed/null-pointer-dereference/ArrayPrototype::method_toString.js
@@ -1,7 +1,8 @@
 function main() { 
 const v1 = [3769255543,3769255543,3769255543,3769255543];
 let {"constructor":v2,"length":v3,"toString":v4,} = v1;
-const v6 = v4["call"]();
+const v6 = [3769255543,3769255543,3769255543,3769255543];
+const v7 = v6.reduce(v4);
 }
 main();
 // CRASH INFO
---
+++ results/crashes/reviewed/null-pointer-dereference/getThisString.js
@@ -0,0 +1,10 @@
+function main() { 
+const v2 = ["i68jdS1zZC"];
+const v3 = "i68jdS1zZC".endsWith;
+const v4 = v2.reduceRight(v3,3769255543);
+}
+main();
+// CRASH INFO
+// ==========
+// TERMSIG: 11
+// STDERR:
