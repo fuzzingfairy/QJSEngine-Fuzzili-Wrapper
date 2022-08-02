e5248a52de657fe793f33a0f3374fdb56ed32e0c
theo morales <thmorale@cisco.com>
2022-08-02 16:36:16 -0500

    defect #8

--- results/crashes/reviewed/needs-more-review/75CAD9060007_flaky.js
+++ results/crashes/reviewed/needs-more-review/75CAD9060007_flaky.js
@@ -1,7 +1,6 @@
 function main() { 
-const v0 = {};
-const v1 = [v0,v0,v0,v0];
-const v3 = v1.splice(-4294967296,-4294967296,v1);
+const v1 = [{},{},{}];
+v1[1] = v1;
 const v4 = v1.toLocaleString();
 }
 main();
